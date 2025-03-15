de6bb07ee153329c5a19ce0e8e903678e052e89d
Mustafa82040 <altunalmustafa2@gmail.com>
2025-03-16 01:20:43 +0300

    Update AdressQuery.sql

--- odev2/AdressQuery.sql
+++ odev2/AdressQuery.sql
@@ -1,10 +1,10 @@
 --create database _LIBRARY
 create table ADDRESSES(
 addressNo int not null primary key identity(1,1),
-country varchar(50),
-city varchar(50),
-street varchar(50),
-neighbourhood varchar(50),
+country varchar(70),
+city varchar(70),
+street varchar(70),
+neighbourhood varchar(70),
 apartNumber int,
 postCode int
 )
